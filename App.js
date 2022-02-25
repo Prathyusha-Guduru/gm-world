fac74a8a033ed8585b3f3fab840aabc329ffe676
Prathyusha-Guduru <prathyushaguduru28@gmail.com>
2022-02-25 18:51:34 +0530

    Update app.js

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    console.log("wgwrg");
+    console.log(require("./assets/icon.png"));
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text numberOfLines={1}>
+                Open up App.js to start working on your app! Making this long
+                long long long so that you know might be a good typing practise
+                tooooo LOL GM BRO
+            </Text>
+            {/* <Image
+                source={{
+                    width: 200,
+                    height: 400,
+                    uri: "https://raw.githubusercontent.com/OMAS-Technocraft/frontend-assets/main/account.png?token=GHSAT0AAAAAABNL7VSVULAR6FYKOHIKBBRAYQX3VLQ",
+                }}
+            /> */}
+            {/* <Image source={require("./icon.png")} /> */}
+            <Image style={styles.img} source={require("./icon.png")} />
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "blue",
+        // alignItems: "center",
+        // justifyContent: "center",
+    },
+    img: {
+        height: "400px",
+        width: "400px",
+    },
 });
